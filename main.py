790a908e562972c662b7ea375ed45895d4e58ac8
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-01-12 12:12:16 +0200

    config: Add project pseudocode

---
+++ main.py
@@ -0,0 +1,10 @@
+from utils import Card
+
+def main():
+    # prompt user for card number
+    # create instance of Card
+    # Validate card through all methods
+    pass
+
+if __name__ == "__main__":
+    main()
