3406a045f129e0446e7c36843a103746c386e017
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-01-13 09:20:50 +0200

    New: Add global functions file

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-from utils import Card
+import utils
+import sys
 
 def main():
-    # prompt user for card number
-    name = str(input("Name (Default = Card): "))
-    try:
-        card_number = int(input("Card Number: "))
-    except Exception as e:
-        print(f"Error: {e}\nInput has to be a number")
-        pass
-        
-        # 4293021017275607
-    # create instance of Card
-    card = Card(card_number, name)
-    # Validate card through all methods
-    print(card)
+    pass
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,12 @@
+"""
+Global functions required by Cardor for card number verification.
+"""
+
+def count_digits(number):
+    pass
+
+def get_firt_number(number):
+    pass
+
+def pass_luhn_test(number):
+    pass
