e23246f031c011b7aabb137919d22479d52f7d29
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-01-13 10:08:56 +0200

    Update: Add split number processing step

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import sys
 def main():
     # Get card number
     try:
-        number = int(sys.argv[2])
+        number = sys.argv[1]
+        # Convert number to Tuple[int]
+        number = utils.split_number(number)
     except Exception as e:
-        print("Input must be a number.\n\nFailed with error: {}".format(e))
-    
-    # Convert number to tuple with int digits for easy maths
-    print(utils.split_number(number))
+        print(
+            "Input must be a number.\nFailed with error: {}".format(e)
+        )
 
     pass
 
--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ Global functions required by Cardor for card number verification.
 """
 
 def split_number(number):
-    num_data = tuple(str(number).split())
+    num_data = str(number).split()
     return (int(i) for i in num_data)
