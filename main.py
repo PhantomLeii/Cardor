9d3918937283d2feb34704e9bc3351033c15cf37
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-01-13 09:25:55 +0200

    Update: Add CLI parsing for card number

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import utils
 import sys
 
 def main():
+    # Get card number
+    try:
+        number = sys.argv[2]
+    except Exception as e:
+        print("Input must be a number.\n\nFailed with error: {}".format(e))
+
+    
+
     pass
 
 if __name__ == "__main__":
