d4ba51ad436e756fb513a9ce7d1ae9e1c6fa111a
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-01-14 08:15:15 +0200

    Update: Add tests for each card amex, mastercard & visa

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-import utils
+from utils import *
 import sys
 
 def main():
     # Get card number
     try:
         number = sys.argv[1]
-        # Convert number to Tuple[int]
-        number = utils.split_number(number)
     except Exception as e:
         print(
             "Input must be a number.\nFailed with error: {}".format(e)
         )
+        return 1
+    
+    # Convert number to Tuple[int]
+    number = split_number(number)
 
-    pass
+    # Run 3 tests simultaneously
+    # Check american express validity
+    if get_first_number(number) == 34 or get_first_number(number) == 37:
+        if len(number) != 15 or not luhn_compliant(number):
+            print("INVALID")
+            return -1
+        
+        print("AMEX")
+        return 0
+
+    elif get_first_number(number) >= 51 and get_first_number <= 55:
+        if len(number) != 16 or not luhn_compliant(number):
+            print("INVALID")
+            return -1
+        
+        print("MASTERCARD")
+        return 0
+    
+    elif str(get_first_number(number)).startswith("4"):
+        if len(number) not in (13, 16) or not luhn_compliant(number):
+            print("INVALID")
+            return -1
+        
+        print("VISA")
+        return 0
+    
+    print("INVALID")      
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,7 +1,54 @@
 """
-Global functions required by Cardor for card number verification.
+Global functions & structures required by Cardor for card number verification.
 """
 
+CONSTANTS = {
+    "amex": (
+        (15,), # digits
+        (34, 37) # startswith
+    ),
+
+    "mastercard": (
+        (16,),
+        (51, 52, 52, 54, 55)
+    ),
+
+    "visa": (
+        (13, 16),
+        (4,)
+    )
+}
+
 def split_number(number):
-    num_data = str(number).split()
-    return (int(i) for i in num_data)
+    store = []
+    for i in number:
+        store.append(int(i))
+    return tuple(store)
+
+def get_first_number(num:iter):
+    return int(str(num[0]) + str(num[1]))
+
+def digit_sum(val):
+    digit_sum = 0
+    if val > 9:
+        digit_sum += sum([int(i) for i in str(val).split()])
+    else:
+        digit_sum = val
+    
+    return digit_sum
+
+def luhn_compliant(num:iter):
+    score = [0,0] # every_other, not_multiplied
+    count = 0
+    for i in num:
+        count += 1
+        if count % 2 == 0:
+            product = i * 2
+            score[0] += digit_sum(product)
+        else:
+            score[1] += i
+
+    if sum(score) % 10 == 0:
+        return True
+    else:
+        return False
