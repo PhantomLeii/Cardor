9fd83fa2d567d747bc283e6fc200aa9ba73ad3f6
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-01-17 11:10:02 +0200

    Update: Add expected function parameter types & return types

--- utils.py
+++ utils.py
@@ -2,24 +2,24 @@
 Global functions & structures required by Cardor for card number verification.
 """
 
-CONSTANTS = {
-    "amex": (
-        (15,), # digits
-        (34, 37) # startswith
-    ),
+# CONSTANTS = {
+#     "amex": (
+#         (15,), # digits
+#         (34, 37) # startswith
+#     ),
 
-    "mastercard": (
-        (16,),
-        (51, 52, 52, 54, 55)
-    ),
+#     "mastercard": (
+#         (16,),
+#         (51, 52, 52, 54, 55)
+#     ),
 
-    "visa": (
-        (13, 16),
-        (4,)
-    )
-}
+#     "visa": (
+#         (13, 16),
+#         (4,)
+#     )
+# }
 
-def split_number(number):
+def split_number(number:iter) -> tuple:
     store = []
     for i in number:
         store.append(int(i))
@@ -28,7 +28,7 @@ def split_number(number):
 def get_first_number(num:iter):
     return int(str(num[0]) + str(num[1]))
 
-def digit_sum(val):
+def digit_sum(val:int) -> int:
     digit_sum = 0
     if val > 9:
         digit_sum += sum([int(i) for i in str(val).split()])
@@ -37,7 +37,7 @@ def digit_sum(val):
     
     return digit_sum
 
-def luhn_compliant(num:iter):
+def luhn_compliant(num:iter) -> bool:
     score = [0,0] # every_other, not_multiplied
     count = 0
     for i in num:
