29664f93ff3814adf10e31689e2008a5d9fcea66
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-01-18 16:38:34 +0200

    Update: Fix digit_sum bug

--- main.py
+++ main.py
@@ -3,27 +3,36 @@ import sys
 
 def main():
     # Prompt user for card number
-    card_number = "4847954053662521"
+    card_number = sys.argv[1].strip()
     
     number = list([int(i) for i in card_number])
 
-    # Check card number validity
-    first_number = get_first_number(number=number)
-    number_length = len(number)
-    
-    if first_number == 34 or first_number == 37:
-        if not luhn_compliant(number) or number_length(number) != 16:
+    # Rejects failed property matches
+    if get_first_number(number=number) == 34 or get_first_number(number=number) == 37:
+        if len(number) != 15 or luhn_compliant(number) is False:
             print("INVALID")
             return 0
         print("AMEX")
         return 0
     
-    elif first_number >= 51 and first_number <= 55:
-        if not luhn_compliant(number) or number_length != 15:
-            pass
-
-    # Print card type
-    return 0
+    elif get_first_number(number=number) >= 51 and get_first_number(number=number) <= 55:
+        if len(number) != 16 or luhn_compliant(number=number) is False:
+            print("INVALID")
+            return 0
+        print("MASTERCARD")
+        return 0
+    
+    elif get_first_number(number=number) == 4:
+        lengths = (13, 16)
+        if len(number) not in lengths or luhn_compliant(number) is False:
+            print("INVALID")
+            return 0
+        print("VISA")
+        return 0
+    
+    else:
+        print("INVALID")
+        return 0
 
 if __name__ == "__main__":
     main()
